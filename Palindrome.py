5911c021f673ebc9e7fa73dddec553e1a1f6f314
Vaishnavi Uttarkar <vaishnaviuttarkar@Vaishnavis-MacBook-Air.local>
2022-12-06 17:56:10 +0530

    Day 21 -> Palindrome

--- Palindrome.py
+++ Palindrome.py
@@ -2,17 +2,33 @@
 
 n=input('Enter a number: ')
 
-def ReverseNumber(n):
+def Palindrome(n):
     l=[]
     e=-1
     for letter in n:
         l.append(n[e])
         e-=1
     l=''.join(l)
-    print(l)
     if n==l:
         print('Palindrome')
     else:
         print('Not a Palindrome')
 
-ReverseNumber(n)
+Palindrome(n)
+
+# Alternate way:
+'''
+n=int(input('Enter a number: '))
+new_n=n
+reversed_no=0
+while n!=0:
+    temp=n%10
+    reversed_no=reversed_no*10+temp
+    n=n//10
+
+print(reversed_no)
+if new_n==reversed_no:
+    print('Palindrome')
+else:
+    print('Not a Palindrome')
+'''
